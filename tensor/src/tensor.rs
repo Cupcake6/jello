b96eb57fafb6fa52b9d2f9161d8b7aed28f1a87e
Cupcake6 <66163408+Cupcake6@users.noreply.github.com>
2026-07-29 11:57:17 +0300

    Implementing `Tensor::num_dims()`

--- tensor/src/tensor.rs
+++ tensor/src/tensor.rs
@@ -9,4 +9,8 @@ impl<B: Backend, T: SupportedDType<B>> Tensor<B, T> {
     pub fn full<S: Into<Shape>>(fill_value: T, shape: S) -> Self {
         Self(B::full(fill_value, shape.into()))
     }
+
+    pub fn num_dims(&self) -> usize {
+        B::num_dims(&self.0)
+    }
 }
