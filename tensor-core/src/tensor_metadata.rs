c95bee564d7e270c80bfddcd2debe1d502030372
Cupcake6 <66163408+Cupcake6@users.noreply.github.com>
2026-08-23 15:24:58 +0300

    Removing unused import

--- tensor-core/src/tensor_metadata.rs
+++ tensor-core/src/tensor_metadata.rs
@@ -1,4 +1,4 @@
-use crate::tensor_metadata::{dimensions::Dimensions, memory_layout::MemoryLayout, shape::Shape};
+use crate::tensor_metadata::{memory_layout::MemoryLayout, shape::Shape};
 
 pub mod dimensions;
 pub mod memory_layout;
