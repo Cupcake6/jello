4a5b695f3b4e3d4f55137e43de96f5044378ccc5
Cupcake6 <66163408+Cupcake6@users.noreply.github.com>
2026-08-23 15:54:14 +0300

    Adding `Indexing` tensor feature

--- tensor-core/src/backend/tensor_features.rs
+++ tensor-core/src/backend/tensor_features.rs
@@ -1,7 +1,22 @@
 use crate::{backend::Backend, dtype::SupportedDType};
 
-pub mod error {}
+pub mod error {
+    #[derive(Debug, thiserror::Error)]
+    pub enum IndexingError {
+        #[error("mismatched number of dimensions: expected {0}, but {1} were provided")]
+        MismatchedNumberOfDimensions(usize, usize),
+
+        #[error("index out of bounds")]
+        IndexOutOfBounds,
+    }
+}
 
 pub trait FlatIter: Backend {
     fn flat_iter<T: SupportedDType<Self>>(tensor: &Self::TensorImpl<T>) -> impl Iterator<Item = T>;
+}
+
+pub trait Indexing: Backend {
+    fn get<T: SupportedDType<Self>>(
+        tensor: &Self::TensorImpl<T>,
+    ) -> Result<T, error::IndexingError>;
 }
