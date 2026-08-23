eb4430776ce5f9b707bfd2c5bda98c8f0658e958
Cupcake6 <66163408+Cupcake6@users.noreply.github.com>
2026-08-23 14:06:44 +0300

    Adding `todo!()` panic instead of wrong behavior when using `Tensor::<Cpu, _>::flat_iter()` on non-contiguous tensors

--- tensor-cpu/src/tensor_features.rs
+++ tensor-cpu/src/tensor_features.rs
@@ -1,13 +1,19 @@
 use crate::CpuBackend as B;
 use tensor_core::{backend::tensor_features::FlatIter, dtype::SupportedDType};
 
-pub struct CpuFlatIter<'a, T: SupportedDType<B>>(std::slice::Iter<'a, T>);
+pub enum CpuFlatIter<'a, T: SupportedDType<B>> {
+    Contiguous(std::slice::Iter<'a, T>),
+    General,
+}
 
 impl<'a, T: SupportedDType<B>> Iterator for CpuFlatIter<'a, T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().copied()
+        match self {
+            Self::Contiguous(it) => it.next().copied(),
+            Self::General => todo!(),
+        }
     }
 }
 
@@ -15,6 +21,10 @@ impl FlatIter for B {
     fn flat_iter<'a, T: SupportedDType<Self>>(
         tensor: &'a Self::TensorImpl<T>,
     ) -> impl Iterator<Item = T> {
-        CpuFlatIter(tensor.data.iter())
+        if tensor.metadata.contiguous() {
+            CpuFlatIter::Contiguous(tensor.data.iter())
+        } else {
+            CpuFlatIter::General
+        }
     }
 }
